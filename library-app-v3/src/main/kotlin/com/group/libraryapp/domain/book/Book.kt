ec4a6f078ee5a231876d499594e3352e33e20bd0
ShaunAUS <86937655+ShaunAUS@users.noreply.github.com>
2022-12-29 10:07:56 +0900

    domain ->kotlin
    domain java -> kotlin으로 변경

---
+++ library-app-v3/src/main/kotlin/com/group/libraryapp/domain/book/Book.kt
@@ -0,0 +1,23 @@
+package com.group.libraryapp.domain.book
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity // JPA entity로 등록하기 위해서느 기본생성자 필요함 -> id 'org.jetbrains.kotlin.plugin.jpa'version'1.6.21'
+class Book(
+
+        val name: String,
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val id: Long? = null,  // 처음 기본값 무조건 null 인것이 맨밑 관례
+) {
+
+    init {
+        if (name.isBlank()) {
+            throw IllegalArgumentException("이름은 비어 있을 수 없습니다")
+        }
+    }
+}
