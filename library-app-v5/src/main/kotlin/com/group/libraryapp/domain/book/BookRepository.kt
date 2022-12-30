420c7f443a8a30a55d8a23a7313a7c9beac267a5
ShaunAUS <86937655+ShaunAUS@users.noreply.github.com>
2022-12-31 00:02:15 +0900

    optional 제거 ,utils 추가 확장함수
    optional 제거 ,utils 추가 확장함수

---
+++ library-app-v5/src/main/kotlin/com/group/libraryapp/domain/book/BookRepository.kt
@@ -0,0 +1,11 @@
+package com.group.libraryapp.domain.book
+
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
+
+interface BookRepository:JpaRepository<Book,Long> {
+
+
+    //Optional 대신 ? 가능 , 지금은 service와의 호환성 때문에 그냥 씀
+    fun findByName(bookName:String):Book?
+}
---
+++ library-app-v5/src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -0,0 +1,51 @@
+package com.group.libraryapp.service.user
+
+import com.group.libraryapp.domain.user.User
+import com.group.libraryapp.domain.user.UserRepository
+import com.group.libraryapp.dto.user.request.UserCreateRequest
+import com.group.libraryapp.dto.user.request.UserUpdateRequest
+import com.group.libraryapp.dto.user.response.UserResponse
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import com.group.libraryapp.utils.fail
+import com.group.libraryapp.utils.findByIdOrThorw
+import org.springframework.data.repository.findByIdOrNull
+
+
+@Service
+class UserService(
+        private val userRepository: UserRepository,  //  매개변수 받는 생성자 자동생성
+
+) {
+
+    //@Transactional 쓸려면 open인 상태, 밑으로 override가능해야하는 상태여야함
+    //id 'org.jetbrains.kotlin.plugin.spring'version'1.6.21'
+    @Transactional
+    fun saveUser(request: UserCreateRequest) {
+        val newUser = User(request.name, request.age)
+        userRepository.save(newUser)
+    }
+
+    @Transactional(readOnly =true)
+    fun getUsers():List<UserResponse>{
+        return userRepository.findAll()
+                .map { user -> UserResponse(user) }
+                //.map { UserResponse(it) }   이 방법도 가능
+
+    }
+
+    // optional 로 반환값이 고정되있는경우 확장함수 사
+    //코틀린-spring => crudRepositoryExtension 이미 존재함 이것을 사용!
+    @Transactional
+    fun updateUserName(request: UserUpdateRequest){
+        //val user = userRepository.findById(request.id).orElseThrow(::IllegalArgumentException)
+        //val user = userRepository.findByIdOrNull(request.id)?: fail()
+        val user = userRepository.findByIdOrThorw(request.id)?: fail()   // 확장함수 한번더
+        user.updateName(request.name)
+    }
+    @Transactional
+    fun deleteUser(name:String){
+        val user = userRepository.findByName(name)?: fail()
+        userRepository.delete(user)
+    }
+}
